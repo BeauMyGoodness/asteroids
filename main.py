3cb528d98f956a04b9f3c6b7e0b59f312f63689c
BeauMyGoodness <Beaubeaufett@gmail.com>
2026-04-01 22:10:46 -0700

    added player object and spawn

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Initialize pygame
     pygame.init()
@@ -20,7 +22,9 @@ def main():
         dt = clock.tick(60) / 1000
         #print(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+    
 
 if __name__ == "__main__":
     main()
